f392bc215b75c5b69efc77b11eb6ba9cf0735f1c
Piotr <piotr.przekaza@hotmail.com>
2021-07-24 13:08:33 +0200

    added basic function to work with api, searching location and check weather condition

--- js/forecastAPI.js
+++ js/forecastAPI.js
@@ -1,1 +1,23 @@
 const API = 'f5FSIBsRKQX2AA7jEWzdjGVIkfA0PSPo';
+
+const searchLocation = async (city) =>{
+    const baseSearch = 'https://dataservice.accuweather.com/locations/v1/cities/search';
+    const query =`?apikey=${API}&q=${city}`
+
+    const response = await fetch(baseSearch + query);
+    const data = await  response.json();
+
+    return data[0];
+};
+
+const getWeatherCondition = async (locationKey)=>{
+    const baseWeather = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${locationKey}?apikey=${API}`;
+
+    const response = await fetch(baseWeather+query);
+    const data = await response.json();
+    console.log(data)
+    return data[0];
+}
+
+getWeatherCondition('2696858')
