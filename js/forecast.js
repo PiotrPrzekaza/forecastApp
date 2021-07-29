eca09b2db54f26d8f019750d0dd76ff7729fa65c
Piotr <piotr.przekaza@hotmail.com>
2021-07-29 18:43:47 +0200

    update UI by weather information

--- js/forecast.js
+++ js/forecast.js
@@ -1,7 +1,22 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 const searchCityForm = $('form');
+const weatherDisplayCard = $('.card');
+const weatherDetails = $('.details');
 
+const updateWeatherDisplay = (data) =>{
+    const location = data.locationDetails;
+    const weather = data.weather;
+
+    weatherDetails.innerHTML = `
+         <h5 class="my-3">${location.EnglishName}</h5>
+         <div class="my-3">${weather.WeatherText}</div>
+         <div class="display-4 my-4">
+              <span>${weather.Temperature.Metric.Value}</span>
+              <span>&deg;${weather.Temperature.Metric.Unit}</span>
+         </div>
+    `
+}
 
 
 const updateLocation = async(city) => {
@@ -18,8 +33,7 @@ searchCityForm.addEventListener('submit', (e)=>{
     e.preventDefault();
     const location = searchCityForm.location.value.trim();
     searchCityForm.reset();
-    updateLocation(location).then(({locationDetails, weather}) =>{
-        console.log(locationDetails);
-        console.log(weather);
+    updateLocation(location).then((data) =>{
+        updateWeatherDisplay(data);
     });
 })
